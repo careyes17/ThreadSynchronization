058e26a293927c32777b6279d48615f13e5d99dd
Carlos <careyes4820@eagle.fgcu.edu>
2019-11-06 11:49:25 -0500

    Initial commit

---
+++ src/Charles.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+class Charles implements Runnable {
+
+  Garden garden;
+  Random rand = new Random();
+
+  public Charles(Garden g) {
+    this.garden = g;
+  }
+
+  public void run() {
+    try {
+      Thread.sleep(rand.nextInt(1000)); // makes the execution more random
+      for (int i = 0; i < 10; i++) {
+        garden.waitToPlant();
+        garden.plant();
+        Thread.sleep(rand.nextInt(500)); // planting
+      }
+    } catch (InterruptedException e) {
+      System.out.println(e);
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
+}
